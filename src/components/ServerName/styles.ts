91efb031ae659876cf94763189b898a4848dd63b
FelipePDS <felipepdasilva66@gmail.com>
2021-05-23 19:10:59 -0300

    changing Server Name component

--- src/components/ServerName/styles.ts
+++ src/components/ServerName/styles.ts
@@ -14,18 +14,25 @@ export const Container = styled.div`
 
     box-shadow: rgba(0, 0, 0, 0.2) 0px 1px 0px 0px;
     z-index: 2;
+
+    cursor: pointer;
+    transition: background-color .2s;
+
+    &:hover {
+        background-color: var(--secondary-outline);
+    }
 `;
 
 export const Title = styled.h1`
-    font-size: 16px;
+    font-size: 15px;
     font-weight: bold;
 
     color: var(--white);
 `;
 
 export const ExpandIcon = styled(ExpandMore)`
-    width: 28px;
-    height: 28px;
+    width: 24px;
+    height: 24px;
 
     color: var(--white);
 
