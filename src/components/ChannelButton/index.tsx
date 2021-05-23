f249da537d29d2eaf390c3d79d7d08aec9181da2
FelipePDS <felipepdasilva66@gmail.com>
2021-05-23 06:20:00 -0300

    adding Channel Button component

---
+++ src/components/ChannelButton/index.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Container, HashtagIcon, InviteIcon, SettingsIcon } from './styles';
+
+export interface Props {
+    channelName: string;
+    selected?: boolean;
+}
+
+const ChannelButton: React.FC<Props> = ({
+    channelName,
+    selected
+}) => {
+    return (
+        <Container className={selected ? 'active' : ''}>
+            <div>
+                <HashtagIcon />
+                <span>{channelName}</span>
+            </div>
+
+            <div className="channel-button-properties">
+                <InviteIcon />
+                <SettingsIcon />
+            </div>
+        </Container>
+    );
+};
+
+export default ChannelButton;
